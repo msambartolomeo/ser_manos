1f5e568b97f923885fb64fbdafe5c4e48110c61e
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2023-09-04 19:16:22 -0300

    add grid

---
+++ lib/design_system/tokens/grid.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+class SerManosGrid extends Padding{
+
+  const SerManosGrid({super.key, required Widget child}) :super(padding: const EdgeInsets.only(left: 16.0, right: 16.0), child: child);
+
+}
