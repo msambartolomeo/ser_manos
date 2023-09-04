deee6ae58f7ea7a2b712f69eb5bb133d0d7f9b4e
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2023-09-04 19:28:20 -0300

    add logos

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Image.asset('assets/logo cuadrado.png'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
