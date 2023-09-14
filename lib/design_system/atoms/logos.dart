f273cd11383f2b8f3d7b44955c9ed71b832620b2
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-09-13 22:17:58 -0300

    add better quality logos

--- lib/design_system/atoms/logos.dart
+++ lib/design_system/atoms/logos.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
-class SquaredLogo extends StatelessWidget{
+class SquaredLogo extends StatelessWidget {
   const SquaredLogo({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/logo cuadrado.png');
+    return Image.asset(
+      'assets/logo cuadrado.png',
+      height: 150,
+      width: 150,
+    );
   }
-
 }
 
-
-class RectangularLogo extends StatelessWidget{
+class RectangularLogo extends StatelessWidget {
   const RectangularLogo({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/logo rectangular.png');
+    return Image.asset(
+      'assets/logo rectangular.png',
+      height: 25,
+    );
   }
-
 }
