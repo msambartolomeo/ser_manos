91fe068bf4a901157deb88b7c4ee64125e06726b
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-09-20 21:56:11 -0300

    add option to align text

--- lib/design_system/tokens/typography.dart
+++ lib/design_system/tokens/typography.dart
@@ -6,59 +6,93 @@ class _BaseTypography extends Text {
   const _BaseTypography(
     String text, {
     super.key,
+    TextAlign align = TextAlign.left,
     Color color = SerManosColor.neutral100,
     TextStyle textStyle = const SerManosTextStyle.heading1(),
   }) : super(
           text,
           style: textStyle,
+          textAlign: align,
         );
 }
 
 class SerManosTypography extends _BaseTypography {
-  SerManosTypography.heading1(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.heading1(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.heading1(color: color),
         );
 
-  SerManosTypography.heading2(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.heading2(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.heading2(color: color),
         );
 
-  SerManosTypography.subtitle1(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.subtitle1(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.subtitle1(color: color),
         );
 
-  SerManosTypography.body1(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.body1(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.body1(color: color),
         );
 
-  SerManosTypography.body2(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.body2(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.body2(color: color),
         );
 
-  SerManosTypography.button(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.button(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.button(color: color),
         );
 
-  SerManosTypography.caption(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.caption(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text,
           textStyle: SerManosTextStyle.caption(color: color),
         );
 
-  SerManosTypography.overline(String text, {super.key, super.color})
-      : super(
+  SerManosTypography.overline(
+    String text, {
+    super.key,
+    super.color,
+    super.align,
+  }) : super(
           text.toUpperCase(),
           textStyle: SerManosTextStyle.overline(color: color),
         );
