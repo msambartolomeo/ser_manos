bd6285eea3d85c6118bc717e6820705e178b8a37
bsquillari <bsquillari@itba.edu.ar>
2023-11-08 11:52:15 -0300

    Add User Geolocation Provider

---
+++ lib/providers/geolocation_provider.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part "generated/geolocation_provider.g.dart";
+
+@riverpod
+Future<GeoPoint> determineGeolocation(DetermineGeolocationRef ref) async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  Position position = await Geolocator.getCurrentPosition();
+  return GeoPoint(position.latitude, position.longitude);
+}
